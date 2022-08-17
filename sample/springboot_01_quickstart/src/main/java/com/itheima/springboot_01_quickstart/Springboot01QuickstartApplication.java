783c8583701c6dba07dbdf26c5c74f4d27450a50
test <email>
2022-08-17 19:40:12 +0800

    commit

---
+++ sample/springboot_01_quickstart/src/main/java/com/itheima/springboot_01_quickstart/Springboot01QuickstartApplication.java
@@ -0,0 +1,14 @@
+package com.itheima.springboot_01_quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01QuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01QuickstartApplication.class, args);
+        System.out.println("===============");
+    }
+
+}
