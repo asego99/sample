85aa24a4fc63dd7f380669e71c9062fafb90d016
test <email>
2022-08-29 15:40:42 +0800

    commit

--- p/springcloud-parent/user-consumer/src/main/java/com/xxx/UserConsumerApplication.java
+++ p/springcloud-parent/user-consumer/src/main/java/com/xxx/UserConsumerApplication.java
@@ -2,9 +2,9 @@ package com.xxx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,8 +18,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 //@EnableEurekaClient
 @EnableDiscoveryClient
-//@EnableCircuitBreaker
-@EnableFeignClients("com.xxx.feign")
+@EnableCircuitBreaker
+//@EnableFeignClients("com.xxx.feign")
 public class UserConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserConsumerApplication.class, args);
--- p/springcloud-parent/user-consumer/src/main/java/com/xxx/controller/UserController.java
+++ p/springcloud-parent/user-consumer/src/main/java/com/xxx/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.xxx.controller;
 
-//import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
-//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import com.xxx.bean.User;
-import com.xxx.feign.UserFeign;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Mr.Li
@@ -19,50 +23,47 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/consumer")
-//@DefaultProperties(defaultFallback = "defaultFallback")
+@DefaultProperties(defaultFallback = "defaultFallback")
 public class UserController {
-//    @Autowired
-//    private RestTemplate restTemplate;
-
-//    @Autowired
-//    private DiscoveryClient discoveryClient;
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Autowired
-    private UserFeign userFeign;
+    private DiscoveryClient discoveryClient;
 
-//    @Bean
-//    public IRule iRule() {
-//        return new RandomRule();
-//    }
+    @Bean
+    public IRule iRule() {
+        return new RandomRule();
+    }
 
 //    @HystrixCommand(fallbackMethod = "fallbackMethod")
 //    @HystrixCommand(fallbackMethod = "fallbackMethod", defaultFallback = "defaultFallback")
-//    @HystrixCommand(defaultFallback = "defaultFallback")
+    @HystrixCommand(defaultFallback = "defaultFallback")
     @RequestMapping("/find/{id}")
     public User queryById(@PathVariable Integer id){
-//        ServiceInstance serviceInstance = discoveryClient.getInstances("user-provide").get(0);
-//
-//        //在user-consumer服务中通过RestTemplate调用user-provider服务
-//        String url = "http://"+ serviceInstance.getHost() +":"+ serviceInstance.getPort() +"/provide/find/"+id;
-//        String url = "http://user-provide/provide/find/"+id;
-//
-//        User user = restTemplate.getForObject(url, User.class);
+        ServiceInstance serviceInstance = discoveryClient.getInstances("user-provide").get(0);
 
-        User user = userFeign.findById(id);
+        //在user-consumer服务中通过RestTemplate调用user-provider服务
+//        String url = "http://"+ serviceInstance.getHost() +":"+ serviceInstance.getPort() +"/provide/find/"+id;
+        String url = "http://user-provide/provide/find/"+id;
+
+        User user = restTemplate.getForObject(url, User.class);
+
+//        User user = userFeign.findById(id);
 
         return user;
     }
 
-//    public User defaultFallback() {
-//        User user = new User();
-//        user.setUsername("全局降级方法调用");
-//        return user;
-//    }
+    public User defaultFallback() {
+        User user = new User();
+        user.setUsername("全局降级方法调用");
+        return user;
+    }
 
-//    public User fallbackMethod(Integer id) {
-//        User user = new User();
-//        user.setUsername("局部降级方法调用");
-//        return user;
-//    }
+    public User fallbackMethod(Integer id) {
+        User user = new User();
+        user.setUsername("局部降级方法调用");
+        return user;
+    }
 
 }
